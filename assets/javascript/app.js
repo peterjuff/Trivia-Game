7bea28c12d576117515593b6adee459fdc8458c6
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2017-12-18 22:44:38 -0600

    clicks begin

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+// add start function that starts timer and shows questions
+$(document).ready(function() {
+
+var timer = 100;    
+
+$("#start").on("click", function() {
+
+})
+
+
+
+
+
+
+
+
+
+})
