8722f5e278458e118b60c30c4a33615139a583a1
Peter Juffernbruch <peterjuffernbruch@gmail.com>
2018-06-03 20:43:27 -0500

    logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,38 +3,43 @@ $(document).ready(function() {
     var questions = [
         {
           quest: "What is the only vowel on a standard keyboard not located in the top row?",
-          answer: ["A", "U", "I", "E"],
-          correct: "A"
+          answer: [" A ", " U ", " I ", " E "],
+          correctA: " A "
         },
         {
           quest: "How many strings are on a cello?",
-          answer: ["5", "17", "4", "6"],
-          correct: "4"
+          answer: [" 5 ", " 17 ", " 4 ", " 6 "],
+          correctA: " 4 "
         },
         {
           quest: "What is the only mammal that cannot jump?",
-          answer: ["Kangaroo", "Dog", "Elephant", "Snake"],
-          correct: "Elephant"
+          answer: [" Kangaroo ", " Dog ", " Elephant ", " Snake "],
+          correctA: " Elephant "
         },
         {
           quest: "How many people have walked on the moon?",
-          answer: ["3", "0", "7", "12"],
-          correct: "12"
+          answer: [" 3 ", " 0 ", " 7 ", " 12 "],
+          correctA: " 12 "
         },
         {
           quest: "How many players are on a baseball team?",
-          answer: ["10", "9", "12", "4"],
-          correct: "9"
+          answer: [" 10 ", " 9 ", " 12 ", " 4 "],
+          correctA: " 9 "
         },
         {
           quest: "What is the chemical symbol of iron?",
-          answer: ["Ir", "Xo", "Mg", "Fe"],
-          correct: "Fe"
+          answer: [" Ir ", " Xo ", " Mg ", " Fe "],
+          correctA: " Fe "
         },
         {
           quest: "Jack the Ripper terrorized which city?",
-          answer: ["London", "Paris", "Philadelphia", "Vienna"],
-          correct: "London"
+          answer: [" London ", " Paris ", " Philadelphia ", " Vienna "],
+          correctA: " London "
+        },
+        {
+          quest: "Which planet is closest to the sun?",
+          answer: [" Mars ", " Venus ", " Mercury ", " Planet X "],
+          correctA: " Mercury "
         }
       ]
 
@@ -61,10 +66,11 @@ $(document).ready(function() {
           countdown = setInterval(quiz.clock, 1000);
           $("#clock").append("<h2>Time Remaining: <span id='timer'>30</span> Seconds</h2>");
           $("#remove").remove();
+
           for (var i = 0; i < questions.length; i++) {
             $("#questDisplay").append("<h2>" + questions[i].quest + "</h2>");
             for (var j = 0; j < questions[i].answer.length; j++) {
-              $("#questDisplay").append("<input type='radio' name='question-" + i + "' value='" + questions[i].answer[j] + "''>" + questions[i].answer[j]);
+              $("#questDisplay").append("<input type='radio' id='radio' class='with-gap' name='question-" + i + "' value='" + questions[i].answer[j] + "''>" + questions[i].answer[j]);
             } 
           }
 
@@ -72,8 +78,8 @@ $(document).ready(function() {
         },
 
         finished: function() {
-          $.each($("input[name='question-0]: checked"), function() {
-            if ($(this).val() === questions[0].correct) {
+          $.each($("input[name='question-0']:checked"), function() {
+            if ($(this).val() === questions[0].correctA) {
               quiz.correct++;
             }
             else {
@@ -81,8 +87,8 @@ $(document).ready(function() {
             }
           });
 
-          $.each($("input[name='question-1]: checked"), function() {
-            if ($(this).val() === questions[1].correct) {
+          $.each($("input[name='question-1']:checked"), function() {
+            if ($(this).val() === questions[1].correctA) {
               quiz.correct++;
             }
             else {
@@ -90,8 +96,8 @@ $(document).ready(function() {
             }
           });
 
-          $.each($("input[name='question-2]: checked"), function() {
-            if ($(this).val() === questions[2].correct) {
+          $.each($("input[name='question-2']:checked"), function() {
+            if ($(this).val() === questions[2].correctA) {
               quiz.correct++;
             }
             else {
@@ -99,8 +105,8 @@ $(document).ready(function() {
             }
           });
 
-          $.each($("input[name='question-3]: checked"), function() {
-            if ($(this).val() === questions[3].correct) {
+          $.each($("input[name='question-3']:checked"), function() {
+            if ($(this).val() === questions[3].correctA) {
               quiz.correct++;
             }
             else {
@@ -108,8 +114,8 @@ $(document).ready(function() {
             }
           });
 
-          $.each($("input[name='question-4]: checked"), function() {
-            if ($(this).val() === questions[4].correct) {
+          $.each($("input[name='question-4']:checked"), function() {
+            if ($(this).val() === questions[4].correctA) {
               quiz.correct++;
             }
             else {
@@ -117,8 +123,8 @@ $(document).ready(function() {
             }
           });
 
-          $.each($("input[name='question-5]: checked"), function() {
-            if ($(this).val() === questions[5].correct) {
+          $.each($("input[name='question-5']:checked"), function() {
+            if ($(this).val() === questions[5].correctA) {
               quiz.correct++;
             }
             else {
@@ -126,14 +132,23 @@ $(document).ready(function() {
             }
           });
 
-          $.each($("input[name='question-6]: checked"), function() {
-            if ($(this).val() === questions[6].correct) {
+          $.each($("input[name='question-6']:checked"), function() {
+            if ($(this).val() === questions[6].correctA) {
               quiz.correct++;
             }
             else {
               quiz.incorrect++;
             }
-          })
+          });
+
+          $.each($("input[name='question-7']:checked"), function() {
+            if ($(this).val() === questions[7].correctA) {
+              quiz.correct++;
+            }
+            else {
+              quiz.incorrect++;
+            }
+          });
 
           this.result();
         },
@@ -144,6 +159,7 @@ $(document).ready(function() {
           $("#finished").append("<h2>Complete!</h2>");
           $("#correctAns").append("<h3>Correct Answers: " + this.correct + "</h3>");
           $("#incorrectAns").append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+          console.log(this.correct)
         }
 
 
